a90b7722aca912a675cf5ce147b7b7ef79850679
Lukmon492 <lukmon492@gmail.com>
2025-05-31 17:30:56 +0100

    interview app

---
+++ src/components/App.jsx
@@ -0,0 +1,136 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  index: 0,
+  // ready, start, active, finish
+  status: "ready",
+  answer: null,
+  point: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataFetched":
+      return { ...state, status: "start", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "failed" };
+    case "start":
+      return { ...state, status: "active" };
+    case "checkAns":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+    default:
+      throw new Error("Unknown Action");
+  }
+};
+
+const App = () => {
+  const [{ questions, status, index, point, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  console.log(questions);
+  const curQuestion = questions.at(index);
+  console.log(curQuestion);
+
+  // Accessing questions from FAKE API
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        dispatch({ type: "dataFetched", payload: data });
+      })
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  const totalQuestions = questions.length;
+  const maxPoint = questions.reduce((prev, cur) => prev + cur.points, 0);
+  // Cheking answer
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {/* Ready Status */}
+        {status === "ready" && <div className="loader-container">Loading</div>}
+
+        {/* Start Status */}
+        {status === "start" && (
+          <>
+            {/* welcome screen */}
+            <div className="start">
+              <h2>Welcome to the Interview</h2>
+              <h4>{totalQuestions} questions to test your FE Mastery</h4>
+              <button
+                className="btn"
+                onClick={() => dispatch({ type: "start" })}
+              >
+                Let's start
+              </button>
+            </div>
+          </>
+        )}
+
+        {/* Active status */}
+        {status === "active" && (
+          <div>
+            {console.log(curQuestion["question"])}
+            <header className="progress">
+              <progress max={totalQuestions}>10</progress>
+
+              <div>
+                <p>
+                  Question {index + 1}/{totalQuestions}
+                </p>
+              </div>
+              <div>
+                <p>
+                  {point}/{maxPoint}
+                </p>
+              </div>
+            </header>
+            <h3>{curQuestion.question}</h3>
+
+            <div className="options">
+              {curQuestion.options.map((option, index) => (
+                <button
+                  disabled={hasAnswered}
+                  className={`btn btn-option ${
+                    hasAnswered
+                      ? index === curQuestion.correctOption
+                        ? "correct"
+                        : "wrong"
+                      : ""
+                  }
+                  ${index === answer ? "answer" : ""}`}
+                  key={index}
+                  onClick={() => dispatch({ type: "checkAns", payload: index })}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {/* when data failed */}
+        {status === "failed" && (
+          <div className="error">
+            <p>💥 There was an error fecthing questions.</p>
+          </div>
+        )}
+      </Main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { FaPeopleRoof } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <FaPeopleRoof size={120} color="#25e2ff" />
+      <h1>React Interview</h1>
+    </header>
+  );
+};
+
+export default Header;
